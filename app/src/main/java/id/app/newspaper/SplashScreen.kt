617396157b282970c0745c80ef49e92f4e1fe5ed
AbdiCahyaNegoro <134830522+AbdiCahyaNegoro@users.noreply.github.com>
2024-12-22 11:36:02 +0700

    Initial commit

---
+++ app/src/main/java/id/app/newspaper/SplashScreen.kt
@@ -0,0 +1,31 @@
+package id.app.newspaper
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splashscreen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setTransition()
+    }
+    private fun setTransition() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Content::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000L)
+    }
+}
